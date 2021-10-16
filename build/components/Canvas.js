15eb405672c4fe62d22db1c5a756655d0b21e9a5
CAZPFITL <cazpfitl@gmail.com>
2021-10-16 15:57:24 -0500

    v0.0.4

--- build/components/Canvas.js
+++ build/components/Canvas.js
@@ -6,7 +6,7 @@ var Canvas = /** @class */ (function () {
      * @returns canvas created
      */
     Canvas.getCanvas = function () {
-        var _a, _b;
+        var _a;
         var game = window.TicTacToe;
         var canvas = (_a = document.querySelector('canvas')) !== null && _a !== void 0 ? _a : document.createElement('canvas');
         var step = game.counters.stepSize;
@@ -14,21 +14,18 @@ var Canvas = /** @class */ (function () {
         var lasDigitH = parseInt(window.innerHeight.toString().slice(-1));
         var Width = (window.innerWidth - step) - lastDigitW;
         var Height = (window.innerHeight - step) - lasDigitH;
-        var border = (window.innerHeight - Height) / 2;
-        canvas.id = (_b = canvas.id) !== null && _b !== void 0 ? _b : 'TicTacToeApp';
-        canvas.width = (Width / step) % 1 === 0 ? Width : Width + step / 2;
-        canvas.height = (Height / step) % 1 === 0 ? Height : Height + step / 2;
-        canvas.style.borderTop = ((Height / step) % 1 === 0 ? border : border + 0.5) + "px solid #333333";
-        Width = (Width / step) % 1 === 0 ? (Width / step) : (Width / step) + 0.5;
-        Height = (Height / step) % 1 === 0 ? (Height / step) : (Height / step) + 0.5;
-        game.canvasBounds = [Width - 2, Height - 2];
+        var WidthRef = Width > Height ? Height : Width;
+        var HeightRef = Height > Width ? Width : Height;
+        canvas.id = 'TicTacToeApp';
+        canvas.width = WidthRef;
+        canvas.height = HeightRef;
+        game.canvasBounds = [WidthRef, HeightRef];
         return canvas;
     };
     /**
      * Create canvas in DOM and Snake Global
      */
     Canvas.createCanvas = function () {
-        console.log('message');
         var game = window.TicTacToe;
         var canvas = game.helpers.getCanvas();
         // let screen = game.helpers.getScreen()
@@ -36,6 +33,49 @@ var Canvas = /** @class */ (function () {
         // document.getElementsByTagName('body')[0].prepend(screen)
         document.getElementsByTagName('body')[0].prepend(canvas);
         game.helpers.requestAnimation();
+    };
+    /**
+     * Request Animation Frame
+     * @param {drawing function} draw
+     */
+    Canvas.requestAnimation = function () {
+        window.requestAnimationFrame(window.TicTacToe.helpers.draw);
+    };
+    /**
+     * Request Animation Frame
+     * @param {drawing function} draw
+     */
+    Canvas.drawBoard = function () {
+        var game = window.TicTacToe;
+        var unitW = game.canvasBounds[0] / 3;
+        var unitH = (game.canvasBounds[1]) / 3;
+        //Horizontal lines
+        game.ctx.beginPath();
+        game.ctx.moveTo((unitW * 3), unitH);
+        game.ctx.lineTo(0, unitH);
+        game.ctx.stroke();
+        game.ctx.beginPath();
+        game.ctx.moveTo((unitW * 3), (unitH * 2));
+        game.ctx.lineTo(0, (unitH * 2));
+        game.ctx.stroke();
+        //Vertical lines
+        game.ctx.moveTo(unitW, (unitH * 3));
+        game.ctx.lineTo(unitW, 0);
+        game.ctx.stroke();
+        game.ctx.beginPath();
+        game.ctx.moveTo((unitW * 2), (unitH * 3));
+        game.ctx.lineTo((unitW * 2), 0);
+        game.ctx.stroke();
+    };
+    /**
+     * draw game
+     */
+    Canvas.draw = function () {
+        window.TicTacToe.helpers.drawBoard();
+        // (<any>window).TicTacToe.helpers.clearCanvas()
+        // (<any>window).TicTacToe.helpers.drawTarget()
+        // (<any>window).TicTacToe.helpers.drawSnake()
+        window.TicTacToe.helpers.requestAnimation();
     };
     return Canvas;
 }());
--- src/components/Canvas.ts
+++ src/components/Canvas.ts
@@ -3,37 +3,33 @@ export default class Canvas {
      * returns a brand new canvas for the game
      * @returns canvas created
      */
-     public static getCanvas(): any {
+    public static getCanvas(): any {
         const game: any = (<any>window).TicTacToe;
         let canvas: any = document.querySelector('canvas') ?? document.createElement('canvas')
         let step: number = game.counters.stepSize
-        
+
         let lastDigitW: number = parseInt(window.innerWidth.toString().slice(-1))
         let lasDigitH: number = parseInt(window.innerHeight.toString().slice(-1))
-        
+
         let Width: number = (window.innerWidth - step) - lastDigitW
         let Height: number = (window.innerHeight - step) - lasDigitH
-        let border: number = (window.innerHeight - Height) / 2
+
+        let WidthRef = Width > Height ? Height : Width
+        let HeightRef = Height > Width ? Width : Height
         
-        canvas.id = canvas.id ?? 'TicTacToeApp'
-        canvas.width = (Width / step) % 1 === 0 ? Width : Width + step / 2
-        canvas.height = (Height / step) % 1 === 0 ? Height : Height + step / 2
-        canvas.style.borderTop = `${(Height / step) % 1 === 0 ? border : border + 0.5}px solid #333333`
-        
-        Width = (Width / step) % 1 === 0 ? (Width / step) : (Width / step) + 0.5
-        Height = (Height / step) % 1 === 0 ? (Height / step) : (Height / step) + 0.5
-        
-        game.canvasBounds = [Width - 2, Height - 2]
-        
+        canvas.id = 'TicTacToeApp'
+        canvas.width = WidthRef
+        canvas.height = HeightRef
+
+        game.canvasBounds = [WidthRef, HeightRef]
+
         return canvas
     }
-    
+
     /**
      * Create canvas in DOM and Snake Global
      */
-     public static createCanvas(): void {
-        console.log('message')
-
+    public static createCanvas(): void {
         const game: any = (<any>window).TicTacToe;
         let canvas = game.helpers.getCanvas()
         // let screen = game.helpers.getScreen()
@@ -41,5 +37,54 @@ export default class Canvas {
         // document.getElementsByTagName('body')[0].prepend(screen)
         document.getElementsByTagName('body')[0].prepend(canvas)
         game.helpers.requestAnimation()
+    }
+
+    /**
+     * Request Animation Frame
+     * @param {drawing function} draw 
+     */
+    public static requestAnimation(): void {
+        (<any>window).requestAnimationFrame((<any>window).TicTacToe.helpers.draw);
+    }
+
+    /**
+     * Request Animation Frame
+     * @param {drawing function} draw 
+     */
+    public static drawBoard(): void {
+        const game: any = (<any>window).TicTacToe;
+        const unitW = game.canvasBounds[0] / 3
+        const unitH = (game.canvasBounds[1]) / 3
+        //Horizontal lines
+        game.ctx.beginPath();
+        game.ctx.moveTo((unitW * 3), unitH);
+        game.ctx.lineTo(0, unitH);
+        game.ctx.stroke();
+        
+        game.ctx.beginPath();
+        game.ctx.moveTo((unitW * 3), (unitH * 2));
+        game.ctx.lineTo(0, (unitH * 2));
+        game.ctx.stroke();
+        
+        //Vertical lines
+        game.ctx.moveTo(unitW, (unitH * 3));
+        game.ctx.lineTo(unitW, 0);
+        game.ctx.stroke();
+
+        game.ctx.beginPath();
+        game.ctx.moveTo((unitW * 2), (unitH * 3));
+        game.ctx.lineTo((unitW * 2), 0);
+        game.ctx.stroke();
+    }
+
+    /**
+     * draw game
+     */
+    public static draw() {
+        (<any>window).TicTacToe.helpers.drawBoard();
+        // (<any>window).TicTacToe.helpers.clearCanvas()
+        // (<any>window).TicTacToe.helpers.drawTarget()
+        // (<any>window).TicTacToe.helpers.drawSnake()
+        (<any>window).TicTacToe.helpers.requestAnimation();
     }
 }
