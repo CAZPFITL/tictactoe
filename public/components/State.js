2b0feaaf2eaaa38b2f1ebbac24e3025e7de9acf8
CAZPFITL <cazpfitl@gmail.com>
2021-10-19 11:36:37 -0500

    v1.0.0

---
+++ public/components/State.js
@@ -0,0 +1,44 @@
+var State = /** @class */ (function () {
+    function State(app) {
+        this.observers = [];
+        this.parentStateOwner = app;
+        this.state = '';
+        this.showNotifications = false;
+        this.add(app);
+    }
+    /**
+     * Add observer to state
+     * @param {Observer to be added} Observer
+     */
+    State.prototype.add = function (Observer) {
+        this.observers.push(Observer);
+        Observer.observable = this.parentStateOwner;
+    };
+    /**
+     * Remove Observer from state
+     * @param {Observer to be removed} Observer
+     */
+    State.prototype.remove = function (Observer) {
+        this.observers = this.observers.filter(function (fx) { return fx != Observer; });
+    };
+    /**
+     * Notify all the observers
+     */
+    State.prototype.notify = function () {
+        var _this = this;
+        this.observers.forEach(function (item) {
+            item.notification(_this.showNotifications);
+        });
+    };
+    /**
+    * Changes the app state from string paramters
+    * @param {State to be applied} state
+    */
+    State.prototype.changeState = function (state) {
+        this.state = state;
+        this.notify();
+    };
+    return State;
+}());
+export default State;
+//# sourceMappingURL=State.js.map
